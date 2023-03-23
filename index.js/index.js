59c6358d2ff352fce52b92482ee9e786f29ea58f
misra2023 <mishazabdi@gmail.com>
2023-03-23 11:05:08 +0300

    completed assignment

---
+++ index.js/index.js
@@ -0,0 +1,50 @@
+//students marks
+let marks = prompt("Enter the student marks");
+
+if (marks >= 79) {
+    console.log("A");
+} else if (marks >= 60 && marks < 79) {
+    console.log("B");
+    } else if (marks >=49 && marks < 59) {
+      console.log("C");
+    } else if (marks >= 40 && marks < 49)  {
+        console.log("D")
+    } else if (marks < 40) {
+        console.log("E")
+    }
+    console.log(marks);
+
+
+
+
+    //speedlimit
+    const speedlimit = 70;
+    const kmPerDemeritPoint = 5;
+
+    function calculaterDemeritPoints(sped) {
+        if (speed <= speedLimit) {
+            console.log("Ok");
+        } else {
+            console.log("License suspended");
+            { eslse {
+                console.log('Demerit points: ${demeritpoints}');
+            } 
+        }
+    }
+    calculateDemeritpoints(60); // ok
+    calculateDemeritpoints(75); // Demerit points: 1
+    calculateDemeritpoints(100; // Demrit points: 6)
+
+
+
+
+
+    //net salary
+    function individualNetSalary(grossSalary) {
+        const tax = 500;
+        const nhif = 500;
+        const nssf - 500;
+        const netSalary = grossSalary - tax - nhif - nssf;
+        return netSalary;
+    }
+    // console.log(individualNetSalary913000));
